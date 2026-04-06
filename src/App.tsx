523c000f48a823bef9f24f48af5dc621cdfb550c
Tom Murphy <tomwademurphy@gmail.com>
2026-04-06 15:07:13 -0400

    Initial hello world Vite app for Cloudflare Pages

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '10vh' }}>
+      <h1>Hello, Cloudflare Pages!</h1>
+      <p>Deployed with Vite + React on Cloudflare Pages.</p>
+    </div>
+  )
+}
